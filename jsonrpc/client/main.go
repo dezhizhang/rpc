7f5b09896028f637f127ee555d4be60a2433b237
dezhizhang <1541609448@qq.com>
2023-08-12 20:07:31 +0800

    feat:jsonrpc

--- jsonrpc/client/main.go
+++ jsonrpc/client/main.go
@@ -7,6 +7,20 @@ import (
 	"net/rpc/jsonrpc"
 )
 
+//func main() {
+//	conn, err := net.Dial("tcp", "localhost:8080")
+//	if err != nil {
+//		panic("连接失败")
+//	}
+//	var reply string
+//	client := rpc.NewClientWithCodec(jsonrpc.NewClientCodec(conn))
+//	err = client.Call("HelloService.Hello", "bobby", &reply)
+//	if err != nil {
+//		panic("调用失败")
+//	}
+//	fmt.Println(reply)
+//}
+
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
--- jsonrpc/server/main.go
+++ jsonrpc/server/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
+//type HelloService struct {
+//}
+//
+//func (s *HelloService) Hello(request string, reply *string) error {
+//	*reply = "hello" + request
+//	return nil
+//}
+//
+//func main() {
+//	listener, _ := net.Listen("tcp", ":8080")
+//	_ = rpc.RegisterName("HelloService", &HelloService{})
+//
+//	for {
+//		conn, _ := listener.Accept()
+//		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+//	}
+//
+//}
+
 type HelloService struct {
 }
 
 func (s *HelloService) Hello(request string, reply *string) error {
-	*reply = "hello" + request
+	*reply = "hello\t" + request
 	return nil
 }
 
 func main() {
 	listener, _ := net.Listen("tcp", ":8080")
-	_ = rpc.RegisterName("HelloService", &HelloService{})
-
+	err := rpc.RegisterName("HelloService", &HelloService{})
+	if err != nil {
+		log.Printf("注册失败%s", err)
+	}
 	for {
 		conn, _ := listener.Accept()
 		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
 	}
-
 }
