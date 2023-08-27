90a419e5355ebdb64f8423fd0c648127a125ce7c
dezhizhang <1541609448@qq.com>
2023-08-27 20:25:16 +0800

    feat:关联插入

--- README.md
+++ README.md
@@ -531,3 +531,28 @@ func main() {
 
 }
 ```
+### 关联插入
+```go
+func main() {
+
+company := model.Company{
+    Name: "晓智科技",
+}
+
+user := model.User{
+    Username: "晓智",
+    Company:  company,
+}
+
+err := driver.DB.Create(&user).Error
+
+if err != nil {
+    log.Printf("插入失败%s", err)
+}
+
+    fmt.Println("插入成功")
+
+}
+
+
+```
--- gorm/main.go
+++ gorm/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"xiaozhi/gorm/driver"
 	"xiaozhi/gorm/model"
@@ -8,17 +9,21 @@ import (
 
 func main() {
 
+	company := model.Company{
+		Name: "晓智科技",
+	}
+
 	user := model.User{
-		Username: "周华建",
-		Company: model.Company{
-			Name: "晓智科技",
-			ID:   1,
-		},
+		Username: "晓智",
+		Company:  company,
 	}
 
 	err := driver.DB.Create(&user).Error
+
 	if err != nil {
-		log.Fatalf("设置失败%s", err)
+		log.Printf("插入失败%s", err)
 	}
+
+	fmt.Println("插入成功")
 
 }
