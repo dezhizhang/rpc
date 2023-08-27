cda56fbe7ba401cb65c56306f0023a4c51571034
dezhizhang <1541609448@qq.com>
2023-08-27 17:29:34 +0800

    feat:update

--- gorm/main.go
+++ gorm/main.go
@@ -9,11 +9,20 @@ import (
 
 func main() {
 
-	var user model.User
-	tx := driver.DB.Model(&user).Where("username = ?", "刘德华20").Find(&user)
-	if tx.Error != nil {
-		log.Fatalf("获取失败%s", tx.Error)
-	}
-	fmt.Println(user)
+	//var user model.User
+	//tx := driver.DB.Where(&model.User{Username: "刘德华14"}).Find(&user)
+	//if tx.Error != nil {
+	//	log.Fatalf("获取失败%s", tx.Error)
+	//}
+	//fmt.Println(user.Username)
 
+	var users []model.User
+	tx := driver.DB.Where(map[string]interface{}{
+		"username": "刘德华14",
+	}).Find(&users)
+
+	if tx.Error != nil {
+		log.Fatalf("查询失败%s", tx.Error)
+	}
+	fmt.Println(users[0].Username)
 }
