4f2cda1506d64c1871800822225e714b151bbd3a
dezhizhang <1541609448@qq.com>
2023-09-20 07:57:59 +0800

    feat:mapping

--- elastic/main.go
+++ elastic/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic/v7"
 	"log"
@@ -14,6 +13,20 @@ type User struct {
 	Age  int    `json:"age"`
 }
 
+const mapping = `
+	"mappings":{
+		"properties":{
+			"name":{
+			"type":"text",
+			"analyzer":"ik_max_word",
+		},
+		"id":{
+			"type":"integer",
+		}
+	}
+}
+`
+
 func main() {
 	host := "http://localhost:9200"
 	logger := log.New(os.Stdout, "xiaozhi", log.LstdFlags)
@@ -21,27 +34,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	q := elastic.NewMatchQuery("address", "street")
-	do, err := client.Search().Index("user").Query(q).Do(context.Background())
+	//q := elastic.NewMatchQuery("address", "street")
+	//do, err := client.Search().Index("user").Query(q).Do(context.Background())
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//for _, value := range do.Hits.Hits {
+	//	var user User
+	//	err := json.Unmarshal(value.Source, &user)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println(user)
+	//}
+	//
+	//user := User{Name: "张德地", Age: 30}
+	//put, err := client.Index().Index("user").Id("3").BodyJson(&user).Do(context.Background())
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(put)
+
+	// 创建索引
+	do, err := client.CreateIndex("user").BodyString(mapping).Do(context.Background())
 	if err != nil {
 		panic(err)
 	}
 
-	for _, value := range do.Hits.Hits {
-		var user User
-		err := json.Unmarshal(value.Source, &user)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(user)
-	}
-
-	user := User{Name: "张德地", Age: 30}
-	put, err := client.Index().Index("user").Id("3").BodyJson(&user).Do(context.Background())
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(put)
+	fmt.Println(do)
 
 }
