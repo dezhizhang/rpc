580135294adbc876a845a8fd20f88dec3d4ddff3
dezhizhang <1541609448@qq.com>
2023-09-20 06:35:52 +0800

    feat: elastic数据插入

--- README.md
+++ README.md
@@ -593,3 +593,37 @@ func main() {
 	fmt.Println("创建成功")
 }
 ```
+### elastic插入表
+```go
+func main() {
+	host := "http://localhost:9200"
+	logger := log.New(os.Stdout, "xiaozhi", log.LstdFlags)
+	client, err := elastic.NewClient(elastic.SetURL(host), elastic.SetSniff(false), elastic.SetTraceLog(logger))
+	if err != nil {
+		panic(err)
+	}
+	q := elastic.NewMatchQuery("address", "street")
+	do, err := client.Search().Index("user").Query(q).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for _, value := range do.Hits.Hits {
+		var user User
+		err := json.Unmarshal(value.Source, &user)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user)
+	}
+
+	user := User{Name: "张德地", Age: 30}
+	put, err := client.Index().Index("user").Id("3").BodyJson(&user).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(put)
+
+}
+```
--- elastic/main.go
+++ elastic/main.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic/v7"
+	"log"
+	"os"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
-	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"), elastic.SetSniff(false))
+	host := "http://localhost:9200"
+	logger := log.New(os.Stdout, "xiaozhi", log.LstdFlags)
+	client, err := elastic.NewClient(elastic.SetURL(host), elastic.SetSniff(false), elastic.SetTraceLog(logger))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(client)
-
 	q := elastic.NewMatchQuery("address", "street")
 	do, err := client.Search().Index("user").Query(q).Do(context.Background())
-
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(do.Hits.Hits)
+	for _, value := range do.Hits.Hits {
+		var user User
+		err := json.Unmarshal(value.Source, &user)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user)
+	}
+
+	user := User{Name: "张德地", Age: 30}
+	put, err := client.Index().Index("user").Id("3").BodyJson(&user).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(put)
 
 }
