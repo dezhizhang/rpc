47d0a62e1cd7f63c59908fb449a1b171d2ef9c93
dezhizhang <1541609448@qq.com>
2023-10-08 08:10:10 +0800

    feat:创建索引

--- elastic/main.go
+++ elastic/main.go
@@ -8,6 +8,12 @@ import (
 	"os"
 )
 
+type User struct {
+	Name string `json:"name"`
+	Sex  string `json:"sex"`
+	Tel  string `json:"tel"`
+}
+
 func main() {
 	host := "http://localhost:9200"
 	logger := log.New(os.Stdout, "xiaozhi", log.LstdFlags)
@@ -16,11 +22,23 @@ func main() {
 		panic(err)
 	}
 
-	do, i, _ := client.Ping(host).Do(context.Background())
+	user := &User{Name: "周华建", Sex: "男", Tel: "15083356190"}
+	do, _ := client.Index().Index("info").Type("user").Id("1001").BodyJson(user).Do(context.Background())
 
-	version, _ := client.ElasticsearchVersion(host)
-	fmt.Println(do, i)
-	fmt.Println("-----------")
-	fmt.Println(version)
+	fmt.Println("-----", do)
+
+	//do, i, _ := client.Ping(host).Do(context.Background())
+
+	//version, _ := client.ElasticsearchVersion(host)
+	//fmt.Println(do, i)
+	//fmt.Println("-----------")
+	//fmt.Println(version)
 
 }
+
+//func main() {
+//	host := "http://localhost:9200"
+//	logger := log.New(os.Stdout, "xiaozhi", log.LstdFlags)
+//	cient, err := elastic.NewClient(elastic.SetURL(host), elastic.SetSniff(false), elastic.SetTraceLog(logger))
+//
+//}
