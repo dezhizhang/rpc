47d510e5893434c8c3411e369855ec069010f31c
dezhizhang <1541609448@qq.com>
2023-10-17 23:12:02 +0800

    feat:proto序列化

--- testing/first_test.go
+++ testing/first_test.go
@@ -1,10 +1,29 @@
 package testing
 
 import (
+	"fmt"
+	"google.golang.org/protobuf/proto"
 	"testing"
+	p "xiaozhi/proto"
 )
 
 func TestName(t *testing.T) {
 	//fmt.Println("hello world")
 	t.Errorf("测度失败")
+}
+
+func TestProto(t *testing.T) {
+	user := p.User{
+		Username: "刘德华",
+		Age:      44,
+	}
+	marshal, err := proto.Marshal(&user)
+	if err != nil {
+		panic(err)
+	}
+
+	newUser := &p.User{}
+	err = proto.Unmarshal(marshal, newUser)
+
+	fmt.Println(newUser)
 }
