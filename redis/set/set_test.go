65de887b6aa7f5836414120161151f989f6cdebf
dezhizhang <1541609448@qq.com>
2023-10-28 22:20:21 +0800

    feat: 可排序索引

--- redis/set/set_test.go
+++ redis/set/set_test.go
@@ -6,54 +6,121 @@ import (
 	"testing"
 )
 
+// 添加集合
 func TestSAdd(t *testing.T) {
 	var err error
 	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		DB:   0,
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
 	_, err = rdb.Ping(ctx).Result()
-	err = rdb.SAdd(ctx, "set", 100).Err()
+	if err != nil {
+		panic(err)
+	}
+	err = rdb.SAdd(ctx, "set", 100, 200, 300, 400, 10, 20).Err()
 	if err != nil {
 		panic(err)
 	}
 	t.Log("添加集合成功")
 }
 
+// 获取集合元素个数
 func TestSCard(t *testing.T) {
 	var err error
 	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		DB:   0,
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
 	_, err = rdb.Ping(ctx).Result()
 	if err != nil {
 		panic(err)
 	}
 	t.Log("连接数据库成功")
-	size, err := rdb.SCard(ctx, "set").Result()
-	if err != nil {
-		panic(err)
+	size, err1 := rdb.SCard(ctx, "set").Result()
+	if err1 != nil {
+		panic(err1)
 	}
 	t.Log(size)
+
 }
 
-func TestSMembers(t *testing.T) {
+// 判断元素是否在集合是
+func TestSIsMember(t *testing.T) {
 	var err error
 	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		DB:   0,
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
 	_, err = rdb.Ping(ctx).Result()
 	if err != nil {
 		panic(err)
 	}
-	values, err := rdb.SMembers(ctx, "set").Result()
+	ok, _ := rdb.SIsMember(ctx, "set", 100).Result()
+	if !ok {
+		t.Log("集合不含令指定元素")
+		return
+	}
+	t.Log("集合包含指定元素")
+}
+
+// 获取集合中所有的元素
+func TestSMembers(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
 	if err != nil {
 		panic(err)
 	}
+	values, err1 := rdb.SMembers(ctx, "set").Result()
+	if err1 != nil {
+		panic(err1)
+	}
 	t.Log(values)
+}
+
+// 删除集合中元素
+func TestSRem(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	err = rdb.SRem(ctx, "set", 100).Err()
+	t.Log("删除成功")
+}
+
+// 随机删除并返回删除的值
+func TestSPop(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	val, _ := rdb.SPop(ctx, "set").Result()
+	t.Log(val)
+	vals, _ := rdb.SPopN(ctx, "set", 5).Result()
+	t.Log(vals)
 }
--- redis/sortedset/sortedset_test.go
+++ redis/sortedset/sortedset_test.go
@@ -6,20 +6,101 @@ import (
 	"testing"
 )
 
+// 添加一个或多个元素到集合，如果元素已经存在则更新分数
 func TestZAdd(t *testing.T) {
+
 	var err error
 	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		DB:   0,
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
 	})
 	_, err = rdb.Ping(ctx).Result()
 	if err != nil {
 		panic(err)
 	}
-	err = rdb.ZAdd(ctx, "sortedset", redis.Z{Score: 2.5, Member: "刘德华"}).Err()
+	err = rdb.ZAdd(ctx, "zAdd", redis.Z{Score: 2.5, Member: "张德志"}).Err()
 	if err != nil {
 		panic(err)
 	}
+	t.Log("插入成功")
+}
 
+// 返回集合元素个数
+func TestZCard(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	size, err1 := rdb.ZCard(ctx, "zAdd").Result()
+	if err1 != nil {
+		panic(err1)
+	}
+	t.Log(size)
+}
+
+// 获取某个区间的值
+func TestZCount(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	size, err1 := rdb.ZCount(ctx, "zAdd", "(1", "5").Result()
+	if err1 != nil {
+		panic(err)
+	}
+	t.Log(size)
+}
+
+func TestZIncrBy(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	err = rdb.ZIncrBy(ctx, "zAdd", 2, "张德志").Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// 返回集合中某个索引范围的元素
+func TestZRange(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	result, err1 := rdb.ZRange(ctx, "zAdd", 0, -1).Result()
+	if err1 != nil {
+		panic(err1)
+	}
+	t.Log(result)
 }
