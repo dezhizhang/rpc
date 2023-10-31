50a360ff7a90853785f10699a893cd2cbd3acaf6
dezhizhang <1541609448@qq.com>
2023-11-01 05:54:49 +0800

    feat: 文档操作

--- elastic/elastic_test.go
+++ elastic/elastic_test.go
@@ -23,8 +23,10 @@ type User struct {
 	Username string    `json:"username"`
 	Nickname string    `json:"nickname"`
 	CreateAt time.Time `json:"create_at"`
+	Desc     string    `json:"desc"`
 }
 
+// 创建索引
 func TestIndexMapping(t *testing.T) {
 	mapping := `
 		{
@@ -66,5 +68,66 @@ func TestIndexMapping(t *testing.T) {
 		return
 	}
 	t.Log("创建mapping成功", do)
+
+}
+
+// 判断索引是否存在
+func TestIndexExists(t *testing.T) {
+	client, err := elastic.NewClient(
+		elastic.SetSniff(false),
+		elastic.SetURL("http://localhost:9200"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	do, err1 := client.IndexExists("user").Do(context.Background())
+	if err1 != nil {
+		panic(err1)
+	}
+	t.Log(do)
+}
+
+// 删除索引
+func TestDeleteIndex(t *testing.T) {
+	client, err := elastic.NewClient(
+		elastic.SetURL("http://localhost:9200"),
+		elastic.SetSniff(false),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	do, err1 := client.DeleteIndex("user").Do(context.Background())
+	if err1 != nil {
+		panic(err1)
+	}
+	t.Log(do)
+
+}
+
+// 添加文档
+func TestCreateDoc(t *testing.T) {
+	user := &User{
+		ID:       1,
+		Username: "张德志",
+		Nickname: "晓智",
+		CreateAt: time.Now(),
+		Desc:     "It从业人员",
+	}
+	client, err := elastic.NewClient(
+		elastic.SetSniff(false),
+		elastic.SetURL("http://localhost:9200"),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	do, err1 := client.Index().Index("user").BodyJson(user).Do(context.Background())
+	if err1 != nil {
+		panic(err1)
+	}
+	t.Log(do)
 
 }
