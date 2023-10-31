8ea5d5ae46d634bb3a81d6c4260cc5d83384c6b9
dezhizhang <1541609448@qq.com>
2023-10-31 23:35:05 +0800

    feat: 创建索引

--- elastic/elastic_test.go
+++ elastic/elastic_test.go
@@ -1,11 +1,13 @@
 package elastic
 
 import (
+	"context"
 	"github.com/olivere/elastic/v7"
 	"testing"
+	"time"
 )
 
-func TestInsert(t *testing.T) {
+func TestClient(t *testing.T) {
 	client, err := elastic.NewClient(
 		elastic.SetSniff(false),
 		elastic.SetURL("http://localhost:9200"),
@@ -14,4 +16,55 @@ func TestInsert(t *testing.T) {
 		panic(err)
 	}
 	t.Log(client)
+}
+
+type User struct {
+	ID       uint      `json:"id"`
+	Username string    `json:"username"`
+	Nickname string    `json:"nickname"`
+	CreateAt time.Time `json:"create_at"`
+}
+
+func TestIndexMapping(t *testing.T) {
+	mapping := `
+		{
+			"settings" : {
+				"number_of_shards" : 1,
+				"number_of_replicas" : 1
+			},
+			"mappings":{
+				"properties":{
+					"nickname":{
+						"type":"text"
+ 					},
+					"username":{
+						"type":"keyword"
+					},
+					"id":{
+						"type":"integer"
+					},
+					"create_at":{
+						"type":"date",
+						"format":"[YYYY-MM-dd HH:mm:ss]"
+					}
+                 }
+			}
+		}
+	`
+	//t.Log(mapping)
+	client, err := elastic.NewClient(
+		elastic.SetSniff(false),
+		elastic.SetURL("http://localhost:9200"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	do, err1 := client.CreateIndex("user").BodyString(mapping).Do(context.Background())
+	if err1 != nil {
+		t.Logf("创建mapping失败%s", err1.Error())
+		return
+	}
+	t.Log("创建mapping成功", do)
+
 }
